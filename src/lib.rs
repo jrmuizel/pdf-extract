330f9e503de8ed12f478266d3da9d5b30bdf5438
Jeff Muizelaar <jrmuizel@gmail.com>
2024-08-29 12:44:38 -0400

    "Type" is optional in Encodings

--- src/lib.rs
+++ src/lib.rs
@@ -494,7 +494,8 @@ impl<'a> PdfSimpleFont<'a> {
                         }
                     }
                 }
-                let name = pdf_to_utf8(encoding.get(b"Type").unwrap().as_name().unwrap());
+                // "Type" is optional
+                let name = encoding.get(b"Type").and_then(|x| x.as_name()).and_then(|x| Ok(pdf_to_utf8(x)));
                 dlog!("name: {}", name);
 
                 encoding_table = Some(table);
