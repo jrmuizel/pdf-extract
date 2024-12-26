cf3866ce044cd6bdf3e615d31c46aa26ab431a89
Jeff Muizelaar <jrmuizel@gmail.com>
2024-12-26 14:55:51 -0500

    Handling missing unicode map entries in type 3 fonts better
    We do the same thing that we do for simple fonts and fallback to the
    encoding.

--- src/lib.rs
+++ src/lib.rs
@@ -818,7 +818,15 @@ impl<'a> PdfFont for PdfType3Font<'a> {
         if let Some(ref unicode_map) = self.unicode_map {
             let s = unicode_map.get(&char);
             let s = match s {
-                None => { panic!("missing char {:?} in map {:?}", char, unicode_map)}
+                None => {
+                    println!("missing char {:?} in unicode map {:?} for {:?}", char, unicode_map, self.font);
+                    // some pdf's like http://arxiv.org/pdf/2312.00577v1 are missing entries in their unicode map but do have
+                    // entries in the encoding.
+                    let encoding = self.encoding.as_ref().map(|x| &x[..]).expect("missing unicode map and encoding");
+                    let s = to_utf8(encoding, &slice);
+                    println!("falling back to encoding {} -> {:?}", char, s);
+                    s
+                }
                 Some(s) => { s.clone() }
             };
             return s
